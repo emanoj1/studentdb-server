148ce6da90572319483f289a147cdf9b1cbdfe2e
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-07-24 10:38:29 +1000

    modify the Student Model
    Add a reference to the institution admin in the student schema to ensure each institutional admin sees only their own student list.

--- src/models/Student.js
+++ src/models/Student.js
@@ -2,15 +2,44 @@
 
 const mongoose = require('mongoose');
 
-const StudentSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  dateOfBirth: { type: Date, required: true },
-  gender: { type: String, required: true },
-  phone: { type: String, required: false },
-  email: { type: String, required: true },
-  address: { type: String, required: true },
-  dateOfEnrollment: { type: Date, required: true },
-  areaOfStudy: { type: String, required: true }
+const studentSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  dateOfBirth: {
+    type: Date,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  dateOfEnrollment: {
+    type: Date,
+    required: true,
+  },
+  areaOfStudy: {
+    type: String,
+    required: true,
+  },
+  adminId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'InstitutionAdmin',
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('Student', StudentSchema);
+module.exports = mongoose.model('Student', studentSchema);
+
