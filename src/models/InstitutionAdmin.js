06cf0fb004e20e147efe53cd40d8fe3d5d56ed3b
Manoj Kumar <37259245+emanoj1@users.noreply.github.com>
2024-07-28 14:38:19 +1000

    Change schema style

--- src/models/InstitutionAdmin.js
+++ src/models/InstitutionAdmin.js
@@ -2,12 +2,31 @@
 
 const mongoose = require('mongoose');
 
-const InstitutionAdminSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  instituteName: { type: String, required: true },
-  instituteRegistrationNumber: { type: String, required: true }
+const institutionAdminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  instituteName: {
+    type: String,
+    required: true
+  },
+  instituteRegistrationNumber: {
+    type: String,
+    required: true
+  }
 });
 
-module.exports = mongoose.model('InstitutionAdmin', InstitutionAdminSchema);
+module.exports = mongoose.model('InstitutionAdmin', institutionAdminSchema);
+
